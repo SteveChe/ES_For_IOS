baa559c2d9666feeb4423dccce94bbb4ccf11eaa
Steve <408835090@qq.com>
2015-07-01 22:54:46 +0800

    联系人主页

--- MyRill/MyRill/RCDChatList/AddressBook/RCDAddressBookViewController.m
+++ MyRill/MyRill/RCDChatList/AddressBook/RCDAddressBookViewController.m
@@ -33,7 +33,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
-    self.navigationItem.title = @"通讯录";
+    self.navigationItem.title = @"联系人";
     
     self.tableView.tableFooterView = [UIView new];
     
